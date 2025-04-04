d7f4f99bf681d05224301bc9440172c966bbbb54
visala123 <visala.k@algorims.com>
2025-04-04 09:36:24 +0800

    terraform vpc to feature

--- terraform-vpc/route-public.tf
+++ terraform-vpc/route-public.tf
@@ -19,7 +19,7 @@ resource "aws_route_table_association" "public-subnet-1-route-table-association"
   route_table_id = aws_route_table.public-route-table.id
 }
 
-/*resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
   subnet_id      = aws_subnet.public-webtier-subnet-2.id
   route_table_id = aws_route_table.public-route-table.id
-}*/
+}
